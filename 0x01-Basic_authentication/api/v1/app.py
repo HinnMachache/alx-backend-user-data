8afbfe03be5de8f9e5e3ecb6c685469ea9155d0b
HinnMachache <bennyhinn253.bh@gmail.com>
2024-09-04 15:44:46 +0000

    Fixed App file to get port and host from env

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -64,4 +64,4 @@ def before_request() -> None:
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host=host, port=int(port))
